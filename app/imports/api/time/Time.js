b71ac94bb636c063441596e810188864bf56295f
Shun <47809066+saitoshi@users.noreply.github.com>
2021-12-04 10:40:49 +0900

    Update Time.js

---
+++ app/imports/api/time/Time.js
@@ -0,0 +1,28 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+class TimeCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'TimeCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      firstName: String,
+      lastName: String,
+      timeIn: Date,
+      timeOut: Date,
+      workedHours: Number,
+      owner: String,
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+export const Times = new TimeCollection();
